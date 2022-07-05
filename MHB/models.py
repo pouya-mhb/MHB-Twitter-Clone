0e3a263c4de97e151859d67129f07243603665b6
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2022-07-05 19:28:54 +0430

    create models

--- MHB/models.py
+++ MHB/models.py
@@ -1,5 +1,52 @@
+from operator import truediv
 from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
 
+class Profile (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,auto_created=True,blank=False,null=False)
+    following = models.ManyToManyField(User, related_name='', on_delete=models.CASCADE,blank=True,null=True)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    username = models.CharField(max_length=20)
+    bio = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    birthday = models.DateField(editable=True)
+    avatar = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.Profile.username
+        # yield Profile.objects.all
+    
+    class Meta ():
+        ordering = ['-created_at']
+
+class Post (models.Model):
+    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE,auto_created=True)
+    liked_by = models.ManyToManyField(Profile,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_created=True,auto_now=True)
+    content = models.TextField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+
+    class Meta ():
+        ordering = ['-created_at']
+
+class Like (models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,auto_created=True)
+    liked_by = models.ManyToManyField(Profile, on_delete=models.CASCADE,auto_created=True)
+    created_at = models.DateTimeField(auto_created=True,auto_now=True)
+
+    class Meta ():
+        ordering = ['-created_at']
+
+class Comment (models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,auto_created=True)
+    commented_by = models.ManyToManyField(Profile,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_created=True,auto_now=True)
+    content = models.TextField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+
+    class Meta ():
+        ordering = ['-created_at']
