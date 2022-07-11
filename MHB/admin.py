e01a5f14b5af29f4c4ab87a94a26d024cddabdc7
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2022-07-11 20:08:21 +0430

    register admin models

--- MHB/admin.py
+++ MHB/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User,Profile,Post,Like,Comment
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
