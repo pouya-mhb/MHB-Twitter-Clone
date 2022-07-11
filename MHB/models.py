95479c2c48e0a3f9f0cf99047cb1b9cf0ecff16e
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2022-07-11 23:33:33 +0430

    modify profile

--- MHB/models.py
+++ MHB/models.py
@@ -4,20 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile (models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,auto_created=True,blank=False,null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,auto_created=True,blank=False,null=True)
     # following = models.ManyToManyField(User,blank=True,null=False)
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
+    firstName = models.CharField(max_length=100, null=True)
+    lastName = models.CharField(max_length=100, null=True)
     username = models.CharField(max_length=20)
-    bio = models.CharField(max_length=150)
+    bio = models.CharField(max_length=150, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    birthday = models.DateField(editable=True)
+    birthday = models.DateField(editable=True, null=True)
     avatar = models.ImageField(blank=True, null=True)
-
-    def __str__(self):
-        return self.Profile.username
-        # yield Profile.objects.all
     
     class Meta ():
         ordering = ['-created_at']
